2b0d5db3413f8e2ff781a5277b36f9372ac1550a
phataii <esanni5@gmail.com>
2026-06-01 16:53:05 +0100

    fix: design fixes

--- obj/Debug/net8.0/nsia.AssemblyInfo.cs
+++ obj/Debug/net8.0/nsia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nsia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d963b3df62d5ea28c69432713ea66834d3b8c096")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0e272a1eca08d21d25ed86fd868987e85efb640")]
 [assembly: System.Reflection.AssemblyProductAttribute("nsia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nsia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
