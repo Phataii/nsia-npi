612b636fea6a820e392eb19911e7de03d18aef16
phataii <esanni5@gmail.com>
2026-06-02 11:24:05 +0100

    feat: new structure

--- obj/Debug/net8.0/nsia.AssemblyInfo.cs
+++ obj/Debug/net8.0/nsia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nsia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28f1a09f41f7a14ddc42f2b5ba504e14414aae27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1841ebc43377c889af600bdbb29e2147ca5843")]
 [assembly: System.Reflection.AssemblyProductAttribute("nsia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nsia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
