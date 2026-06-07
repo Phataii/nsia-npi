4cd6ee66c56dbe2e7c8925ad2f09eb6993039f89
phataii <esanni5@gmail.com>
2026-06-07 10:11:08 +0100

    fix: phone input field

--- obj/Debug/net8.0/nsia.AssemblyInfo.cs
+++ obj/Debug/net8.0/nsia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nsia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47fd3051a88a8b1038c2f02376abd27d127f64c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+156ae87ec110bdee56febcb16556c1e608f323fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("nsia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nsia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
