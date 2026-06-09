7c889b74faa8c2dd373fe8d5d907d710ec6783f9
phataii <esanni5@gmail.com>
2026-06-09 09:15:18 +0100

    admin dashboard fix

--- obj/Debug/net8.0/nsia.AssemblyInfo.cs
+++ obj/Debug/net8.0/nsia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("nsia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce5efbeb678b69589fd2a88188e58779b1a19772")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb882a42b105a7ad2ee3e5692e8e1a6741326f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("nsia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("nsia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
